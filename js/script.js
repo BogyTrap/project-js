6cbab77a6c14af0f3449dd453a4831fc3c93f5bf
BogyTrap <zagorodniybogdan2@gmail.com>
2021-10-18 18:12:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm('Are you here?');
+
+//const answer = prompt('Are you 18?', '18');
+
+/* const answers = [];
+
+answers[0] = prompt('Are you 18?', '');
+answers[1] = prompt('Your name?', '');
+answers[2] = prompt('Where are you from?', '');
+
+document.write(answers); */
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`); */
+
+/* const user = 'Ron';
+
+alert(`Hello, ${user}`); */
+
+/* let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr, decr); */
+
+
+/* console.log(2 * 4 === +'8'); */
+
+const isChecked = true,
+    isClosed = true;
+console.log(isChecked && isClosed);
