373fa2bb9c0ac0108924b4897b60b8bcd46c4dbc
BogyTrap <zagorodniybogdan2@gmail.com>
2021-10-21 17:49:23 +0300

    practice number 1

--- js/script.js
+++ js/script.js
@@ -33,6 +33,29 @@ console.log(incr, decr); */
 
 /* console.log(2 * 4 === +'8'); */
 
-const isChecked = true,
+/* const isChecked = true,
     isClosed = true;
-console.log(isChecked && isClosed);
+console.log(isChecked && isClosed); */
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
